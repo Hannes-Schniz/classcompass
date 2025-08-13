a7c6ed4b7120719f24370298f0daa07df9332e7c
hannes <hannes.schniz@gmx.de>
2025-08-13 22:28:22 +0200

    added timer

--- main/athena.py
+++ main/athena.py
@@ -1,8 +1,19 @@
 from schedule.untis_connector import exporter
 from configReader import configExtract
+import os
 
-ex = exporter()
+untis_exporter = exporter()
 
 conf = configExtract("config.json").conf
 
-print(ex.getData(start="2025-07-21", end="2025-07-25", classID=conf['classID'], verbose=True))
+MINUTECOUNTERVAR = "athena_counter"
+
+minute = os.environ[MINUTECOUNTERVAR]
+
+if minute > 0: 
+    os.environ[MINUTECOUNTERVAR] = minute - 1
+    quit(0)
+
+os.environ[MINUTECOUNTERVAR] = conf['weeksAhead']
+
+untisData = untis_exporter.getData(start="2025-07-21", end="2025-07-25", classID=conf['classID'], verbose=True)
