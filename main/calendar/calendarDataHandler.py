6bcd9ba2c3bd95877ff793fb0aabc768356771bd
hannes <hannes.schniz@gmx.de>
2025-09-15 17:48:40 +0200

    added getData and calendarDataHandler

---
+++ main/calendar/calendarDataHandler.py
@@ -0,0 +1,18 @@
+from google_cal_connector import googleCalCon
+from database.sqliteConnector import plutus
+
+class handler():
+    
+    classes=None
+    diffs=None
+    
+    def getData(self):
+        database = plutus()
+        database.connect()
+        
+        batchID = database.getNewBatchID() - 1
+        
+        self.classes = database.getClasses(batchID=batchID)
+        
+        print(self.classes)
+        return 0
