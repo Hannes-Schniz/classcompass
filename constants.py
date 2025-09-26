9ce5a9e6af69210f78121ca871f5a2c4fc08cf5f
Hannes Schniz <contact@hannesschniz.de>
2025-09-26 23:57:56 +0200

    forgot constants.py

---
+++ constants.py
@@ -0,0 +1,90 @@
+from enum import Enum
+
+
+
+
+######################################
+# 
+# Logging
+#
+######################################
+
+class logLevel(Enum):
+    ERROR = "ERR"
+    WARNING = "WRN"
+    INFO = "INF"
+
+######################################
+# 
+# Config
+#
+######################################
+
+class cfgParams(Enum):
+    CLASSID="classID"
+    COLORSCHEME="color-scheme"
+    PRIMARYCOLOR="primary"
+    CANCELLEDCOLOR="cancelled"
+    CHANGEDCOLOR="changed"
+    EXAMCOLOR="exam"
+    WEEKSAHEAD="weeksAhead"
+    MAINTENANCE="maintenance"
+    SHOWCANCELLED="showCancelled"
+    SHOWCHANGES="showChanges"
+    HISTORY="history"
+    
+class envFile(Enum):
+    CALENDARID="calendarID"
+    COOKIE="cookie"
+    SCHOOL="anonymous-school"
+    BOTTOKEN="telegramToken"
+    CHAT="telegramChat"
+    
+class credsParams(Enum):
+    TYPE="type"
+    PROJECT="project_id"
+    PRIVATEKEYID="private_key_id"
+    PRIVATEKEY="private_key"
+    EMAIL="client_email"
+    CLIENTID="client_id"
+    URIAUTH="auth_uri"
+    URITOKEN="token_uri"
+    CERTPROVIDER="auth_provider_x509_cert_url"
+    CERTCLIENT="client_x509_cert_url"
+    DOMAIN="universe_domain"
+
+######################################
+# 
+# Environment
+#
+######################################
+
+class envParams(Enum):
+    CLASSID= "CLASS_ID"
+    PRIMARYCOLOR= "COLOR_PRIMARY"
+    CANCELLEDCOLOR="COLOR_CANCELLED"
+    CHANGEDCOLOR="COLOR_CHANGED"
+    EXAMCOLOR="COLOR_EXAM"
+    WEEKSAHEAD="WEEKS_AHEAD"
+    MAINTENANCE="MAINTENANCE"
+    SHOWCANCELLED="SHOW_CANCELLED"
+    SHOWCHANGED="SHOW_CHANGES"
+    HISTORY="HISTORY_COUNT"
+
+######################################
+# 
+# MISC
+#
+######################################
+
+class mapBoolean(Enum):
+    "true" = True
+    "1" = True
+    "yes" = True
+    "on" = True
+    "false" = False
+    "0" = False
+    "no" = False
+    "off" = False
+
+    
