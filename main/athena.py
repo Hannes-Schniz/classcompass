e1723e0028739b66a42a3b5374c5ed1f0c4608c5
hannes <hannes.schniz@gmx.de>
2025-09-29 19:15:20 +0200

    removed unused imports

--- main/athena.py
+++ main/athena.py
@@ -3,12 +3,10 @@ from _calendar.calendarDataHandler import calendarHandler
 from configReader import configExtract
 from datetime import datetime, timedelta, timezone
 from _maintenance.dbmaint import maintenance
-import os
 
 conf = configExtract("config.json").conf
 dataHandler = apiHandler() 
 calendar = calendarHandler(int(conf['weeksAhead']))
-database = plutus()
 
 for i in range(int(conf['weeksAhead'])):
     currDate = (datetime.now(timezone.utc) + timedelta(days=i*7) ).strftime('%Y-%m-%d')
